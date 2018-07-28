c2e31d88124f05dbc083c618d89a1a5a6ac0167a
Ozal Khan <ozalkhan196@gmail.com>
2018-07-28 17:56:27 -0400

    async call works with async await also using external db for async calls

--- schema/schema.js
+++ schema/schema.js
@@ -1,22 +1,9 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
+// const _ = require('lodash'); used for static list of users
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 //GQL data types
-
-//dummy data
-const users = [
-  {
-    id: 1,
-    firstName: 'Oz',
-    age: 27
-  },
-  {
-    id: 2,
-    firstName: 'Serena',
-    age: 23
-  }
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -33,12 +20,19 @@ const RootQuery = new GraphQLObjectType({
     user: {
       type: UserType,
       args: { id: { type: GraphQLInt } },
-      resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+      async resolve(parentValue, args) {
+        try {
+          const res = await axios.get(`http://localhost:3000/users/${args.id}`);
+          return res.data;
+        } catch (err) {
+          console.log(err);
+        }
       }
     }
   }
 });
+
+//root query tells graphQL where to enter the graph
 
 module.exports = new GraphQLSchema({
   query: RootQuery
