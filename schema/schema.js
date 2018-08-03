8e9bba67b314e976bcea861cf89ebac022397c09
Ozal Khan <ozalkhan196@gmail.com>
2018-08-03 16:00:27 -0400

    bidirectional relationship made

--- schema/schema.js
+++ schema/schema.js
@@ -2,16 +2,43 @@ const graphql = require('graphql');
 const axios = require('axios');
 // const _ = require('lodash'); used for static list of users
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
 //GQL data types
 
+//circular reference between two types
+//we cant switch where they are defined because the variable is used before being defined due to const.
+//probably would be undefined if we used var
+// note the fields method on CompanyType it is now a function that returns the object that fields once was
+//we use closure to the function is run after the file is executed
+//this makes it so that both CompanyType and UserType are both defined and then graphQL will then use both types despite the circular reference.
+//in the resolve function we go to companies/id/users to get a list of users
+// _+_+_+_we use graphQLList so we can get a list of users since it is a one(Company) to many(users) __+_+_+_+
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLInt },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    user: {
+      type: new GraphQLList(UserType),
+      async resolve(parentValue, args) {
+        try {
+          const userInfo = await axios.get(
+            `http://localhost:3000/companies/${parentValue.id}/users`
+          );
+          return userInfo.data;
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
