faf43f62c19dc7687eb7b6c08f3e628825c79354
Ozal Khan <ozalkhan196@gmail.com>
2018-08-04 19:30:40 -0400

    gitignore added

--- graphQL/index.js
+++ graphQL/index.js
@@ -1,7 +1,7 @@
 const { GraphQLSchema } = require('graphql');
 const mutation = require('./operations/mutation');
 const RootQuery = require('./operations/rootQuery');
-
+//see root query and mutation
 //root file for all graphql operations and schema
 
 module.exports = new GraphQLSchema({
